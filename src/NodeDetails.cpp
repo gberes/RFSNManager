ae4bcd5c1beb42be8c6cf5384b7b9f0ed8a178cc
Gabor Beres <gaborr.b@gmail.com>
2014-05-21 12:41:18 +0200

    asd

--- src/NodeDetails.cpp
+++ src/NodeDetails.cpp
@@ -11,11 +11,14 @@
 #include <Wt/WPanel>
 #include <Wt/WStackedWidget>
 #include <Wt/WText>
+#include <Wt/WTable>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/foreach.hpp>
 #include <sstream>
+#include <vector>
 #include <iostream>
+#include <boost/date_time.hpp>
 #include "RFSNManager.h"
 
 namespace RFSNMAN {
@@ -39,12 +42,37 @@ void NodeDetails::responseArrived(boost::system::error_code err, const Wt::Http:
 	std::istringstream is(response.body());
 	read_json(is, result);
 
+	struct SensorDataTable {
+		int type;
+		std::vector<boost::posix_time::ptime> timestamps;
+		std::vector<float> values;
+	};
+
 	BOOST_FOREACH(boost::property_tree::ptree::value_type &v,result.get_child("sensordatas")) {
+		SensorDataTable sdt;
+		sdt.type = v.second.get<int>("type");
 	    BOOST_FOREACH(boost::property_tree::ptree::value_type &v2, v.second.get_child("values."))
 	    {
-	    	new Wt::WText(v2.second.get<std::string>("value"), manager->root());
+	    	sdt.values.push_back(v2.second.get<float>("value"));
+	    	sdt.timestamps.push_back(v2.second.get<boost::posix_time::ptime>("timestamp"));
+	    	//new Wt::WText(v2.second.get<std::string>("value"), manager->root());
 	    }
+
+		Wt::WTable *table = new Wt::WTable(manager->root());
+		table->setHeaderCount(1);
+		table->setWidth("30%");
+		table->elementAt(0, 0)->addWidget(new Wt::WText("Timestamp"));
+		table->elementAt(0, 1)->addWidget(new Wt::WText("Value"));
+
+		for (unsigned i = 0; i < sdt.values.size(); ++i) {
+		    int row = i + 1;
+
+		    table->elementAt(row, 0)->addWidget(new Wt::WText(Wt::WString(to_simple_string(sdt.timestamps.at(i)))));
+		    table->elementAt(row, 1)->addWidget(new Wt::WText(Wt::WString::fromUTF8("{1}").arg(sdt.values.at(i))));
+		}
 	}
+
+
 	Wt::WApplication::instance()->triggerUpdate();
 }
 void NodeDetails::showRequestErrorMessage(std::string msg) {
